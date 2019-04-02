d31401b8031778fc808b5094278d4a62e5e4518a
carloseduardoc <carlos.conceicao@acad.pucrs.br>
2019-04-01 21:51:45 -0300

    Inclusão de atributos na classe categoria e usuário.

--- app/src/main/java/carlos/robson/easyprice/Dominio/Categoria.java
+++ app/src/main/java/carlos/robson/easyprice/Dominio/Categoria.java
@@ -1,4 +1,19 @@
 package carlos.robson.easyprice.Dominio;
 
 public class Categoria {
+
+    private int codigoCategoria;
+    private String nomeCategoria;
+
+    public int getCodigoCategoria(){
+        return this.codigoCategoria;
+    }
+
+    public String getNomeCategoria(){
+        return this.nomeCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria){
+        this.codigoCategoria = codigoCategoria;
+    }
 }
--- app/src/main/java/carlos/robson/easyprice/Dominio/Usuario.java
+++ app/src/main/java/carlos/robson/easyprice/Dominio/Usuario.java
@@ -1,4 +1,15 @@
 package carlos.robson.easyprice.Dominio;
 
 public class Usuario {
+
+    private String nome;
+    private String cpf;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
 }
